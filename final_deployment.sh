df0e6fc566926457b5a0e333fa2f86fa1f4b4633
Joshua Ipock <joshuaipock4@gmail.com>
2026-07-04 04:34:10 -0500

    chore: reduce strategy_size to small to comply with lite account limits

--- final_deployment.sh
+++ final_deployment.sh
@@ -65,6 +65,7 @@ curl -s -X POST "https://api.${REGION}.codeengine.cloud.ibm.com/v2/projects/${PR
     \"source_revision\": \"shopify-integration-822303029735242017\",
     \"source_secret\": \"retail-swarm-git-auth\",
     \"strategy_type\": \"dockerfile\",
+    \"strategy_size\": \"small\",
     \"output_image\": \"$REGISTRY_SERVER/$NAMESPACE/$IMAGE_NAME:latest\",
     \"output_secret\": \"retail-swarm-registry-secret\"
   }" | python3 -m json.tool
